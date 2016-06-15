fd79c084f085f7ab1a3be0bc0370638b280a5a30
Shaws <seanrichardparker@gmail.com>
2016-06-15 14:43:29 -0600

    first commit

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,82 +1,44 @@
-var num1
-var num2
-var operator
-var display = document.getElementById('display');
+// array of options
+var weapon = ["rock", "paper", "scissors"];
+var pick;
+// var play;
 
+// store user selection
+function userPick(choice) {
+  pick = choice;
+  // clears error message if it is full
+  document.getElementById('error').innerHTML = "";
+  document.getElementById('user-choice').innerHTML = "You chose: " + choice;
 
-function getNum(n){
-  if (num1 == null) {
-    num1 = n;
-  } else {
-    num2 = n;
+  switch (pick) {
+    case 'rock':
+    userPick(choice);
+    var choice = weapon[0];
+    break;
+    case 'paper':
+    userPick(choice);
+    var choice = weapon[1];
+    break;
+    case 'scissors':
+    userPick(choice);
+    var choice = weapon[2];
+    break;
   }
-  display.value = n;
+
+
+}
+// randomize computer selection
+function computerPick() {
+  Math.random(Math.floor() * 3);
 }
 
-// Do maths
-function add(){
-  display.value = num1 + num2;
-}
-function subtract(){
-  display.value = num1 - num2;
-}
-function multiply(){
-  display.value = num1 * num2;
-}
-function divide(){
-  display.value = num1 / num2;
-}
 
-// Set an if statement that concatenate digits into num1 or num2 if an operator hasn't been picked yet.
-
-function getOperator(o) {
-  operator = o;
-  display.value = o;
-}
-
-function calculate() {
-  if (!num1 || !num2) {
-    alert("You must pick a number!")
-  } else{
-    switch(operator) {
-      case '+':
-        add();
-        break;
-      case '-':
-        subtract();
-        break;
-      case '*':
-        multiply();
-        break;
-      case '/':
-        divide();
-        break;
-    }
-
-    //TODO erase();
-    // create a Clear button
-    // Replace num1 with the result of the current equation
-    // Set num2 and operator to null
-
-  }
-}
-
-function erase() {
-  num1 = null;
-  num2 = null;
-  operator = null;
-}
-
+// compare computer pick and user pick to get a result
+// function play{
 //
+// }
 
-// Let it get as many digits as the user hits until an operator is pressed.
-// Show the user input on the 'screen'
-
-// Get the operator
-// Show the user input and operator on the 'screen' ()
-
-// Get second user input
-// Let it get as many digits as the user hits until an operator is pressed.
-// Show user input on screen along with the operator.
-
-// Getting user inputs should be a function with a loop that only stops when the equals sign is pressed.
+// If they pick the orange button they automatically lose
+function orange() {
+  document.getElementById('error').innerHTML = "You done messed up A-a-ron! What do you mean shoot? Nobody brought a gun to this fight.";
+}
