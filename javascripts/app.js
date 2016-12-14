d941d671416d9730d10e6a4708afaf22cdbedb41
Shaws <seanrichardparker@gmail.com>
2016-12-14 00:17:31 -0500

    connecting functions, improved validation, better appearance

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,44 +1,85 @@
 // array of options
 var weapon = ["rock", "paper", "scissors"];
-var pick;
+var choice = "not selected";
 // var play;
-
 // store user selection
-function userPick(choice) {
-  pick = choice;
-  // clears error message if it is full
+
+// erase functions
+function clearErrors() {
   document.getElementById('error').innerHTML = "";
-  document.getElementById('user-choice').innerHTML = "You chose: " + choice;
-
-  switch (pick) {
-    case 'rock':
-    userPick(choice);
-    var choice = weapon[0];
-    break;
-    case 'paper':
-    userPick(choice);
-    var choice = weapon[1];
-    break;
-    case 'scissors':
-    userPick(choice);
-    var choice = weapon[2];
-    break;
-  }
-
-
 }
+function clearPick() {
+  document.getElementById('user-choice').innerHTML = "";
+  choice = "not selected";
+  return choice;
+}
+function clearResults() {
+  document.getElementById('computer-choice').innerHTML = "";
+  document.getElementById('outcome').innerHTML = "";
+}
+
+// pick weapon function // runs the whole game
+function pick (c) {
+  // clears error message if it is full
+
+
+  document.getElementById('user-choice').innerHTML = "You chose: " + c;
+  choice = c;
+  return choice;
+}
+
+function play () {
+  var computer = computerPick();
+
+  if (choice == "not selected") {
+    document.getElementById('error').innerHTML = "<p class='mistake'>Pick a weapon dummy!</p>";
+    clearResults();
+  } else {
+    var randchoice = computerPick();
+
+
+    // user selects rock
+  if (choice == "rock" && randchoice == "scissors") {
+    document.getElementById('outcome').innerHTML = "<p class='win'>Rock smashes scissors!<br />You win!</p>";
+  } else if (choice == "rock" && randchoice == "paper") {
+    document.getElementById('outcome').innerHTML = "<p class='lose'>Paper covers rock!<br />You lose!</p>";
+  } else if (choice == "rock" && randchoice == "rock") {
+    document.getElementById('outcome').innerHTML = "<p class='tie'>Tie!<br />You both lose!</p>";
+    // user selects paper
+    debugger
+  } else if (choice == "paper" && randchoice == "scissors") {
+    document.getElementById('outcome').innerHTML = "<p class='win'>Paper covers rock!<br />You win!</p>";
+  } else if (choice == "paper" && randchoice == "paper") {
+    document.getElementById('outcome').innerHTML = "<p class='lose'>Scissors cut paper!<br />You lose!</p>";
+  } else if (choice == "paper" && randchoice == "rock") {
+    document.getElementById('outcome').innerHTML = "<p class='tie'>Tie!<br />You both lose!</p>";
+    // user selects scissors
+  } else if (choice == "scissors" && randchoice == "scissors") {
+    document.getElementById('outcome').innerHTML = "<p class='win'>Scissors cut paper!<br />You win!</p>";
+  } else if (choice == "scissors" && randchoice == "paper") {
+    document.getElementById('outcome').innerHTML = "<p class='lose'>Rock smashes scissors!<br />You lose!</p>";
+  } else if (choice == "scissors" && randchoice == "rock") {
+    document.getElementById('outcome').innerHTML = "<p class='tie'>Tie!<br />You both lose!</p>";
+  } else {
+    document.getElementById('outcome').innerHTML = "<p class='error'>Something went wrong!</p>";
+  }
+ }
+  return computer;
+}
+
 // randomize computer selection
 function computerPick() {
-  Math.random(Math.floor() * 3);
+  var randomize = weapon[Math.floor(Math.random() * 3)];
+  document.getElementById('computer-choice').innerHTML = "Computer chose: " + randomize;
+  return randomize;
 }
 
 
 // compare computer pick and user pick to get a result
-// function play{
-//
-// }
+
 
 // If they pick the orange button they automatically lose
 function orange() {
-  document.getElementById('error').innerHTML = "You done messed up A-a-ron! What do you mean shoot? Nobody brought a gun to this fight.";
+  document.getElementById('error').innerHTML = "<p class='mistake'>You done messed up A-a-ron! What do you mean shoot? Nobody brought a gun to this fight!!</p>";
+  clearResults();
 }
